d005ca60f86f445fe2a2ee67d2d9fcfec7b8401b
agusgutierrezz <agusgutierrezperez@gmail.com>
2020-05-24 17:57:25 +0200

    Updates

---
+++ precache-manifest.d4eeecffadb763833f790fc4ba8001ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1804b21a4cd26850f1804be296621f",
+    "url": "/findAgus/index.html"
+  },
+  {
+    "revision": "792d86b249fc192286f0",
+    "url": "/findAgus/static/css/main.16924066.chunk.css"
+  },
+  {
+    "revision": "e4c4a9b2a964860f446d",
+    "url": "/findAgus/static/js/2.82dac088.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/findAgus/static/js/2.82dac088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792d86b249fc192286f0",
+    "url": "/findAgus/static/js/main.81bcfbb9.chunk.js"
+  },
+  {
+    "revision": "6bc58eef28acca9fb2ee",
+    "url": "/findAgus/static/js/runtime-main.c4952ec2.js"
+  },
+  {
+    "revision": "6371439a616e372183ca078bf4e73e76",
+    "url": "/findAgus/static/media/maqueta.6371439a.jpg"
+  },
+  {
+    "revision": "f03424d8ecd1274baadaf123eb4e15ba",
+    "url": "/findAgus/static/media/maquetaFeriate.f03424d8.jpg"
+  },
+  {
+    "revision": "d53b61a484a1c2048df8636645bf1333",
+    "url": "/findAgus/static/media/maquetaTutti.d53b61a4.jpg"
+  },
+  {
+    "revision": "18dea1b4470f0199004931a9a294c367",
+    "url": "/findAgus/static/media/profile.18dea1b4.jpeg"
+  }
+]);
